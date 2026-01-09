a2b7ae189f176657290dbbb1611277dd3e66c03d
InfraErione <infraestrutura@erione.com.br>
2026-01-09 16:42:14 -0300

    update server

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import { version } from "./package.json";
+import { strict } from "assert";
 
 function writeVersionPlugin() {
   return {
@@ -21,6 +22,11 @@ function writeVersionPlugin() {
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
+  server: {
+    host:"0.0.0.0",
+    port: 5174,
+    strictPort: true,
+  },
   build: {
     assetsDir: "resources",
   },
